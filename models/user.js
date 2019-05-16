d890df40afad5ca3771499537a48e65027c3ac77
Aiello <aiello@aiellos-mbp.attlocal.net>
2019-05-16 17:49:51 -0400

    Created JS and CSS folders inside of the public folders. Added app.js to the JS folder.

--- models/user.js
+++ models/user.js
@@ -3,7 +3,9 @@
 //=============
 const mongoose = require('mongoose');
 
-//Schema
+//=========
+// SCHEMA
+//=========
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: {type: String, required: true},
