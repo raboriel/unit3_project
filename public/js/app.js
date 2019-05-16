150bed5d51fda6e92a0bb433a0366fa716261c16
Aiello <aiello@aiellos-mbp.attlocal.net>
2019-05-16 18:07:42 -0400

    Added a function that allows you to add items to the database.

--- public/js/app.js
+++ public/js/app.js
@@ -7,5 +7,23 @@ const app = angular.module('MarketPlace', []);
 //CONTROLLER
 //===========
 app.controller('MyController', ['$http', function($http){
-  this.testing = 'This is working';
+
+//Function to create an item and add it to the database
+  this.createItem = function(){
+      $http({
+        method:'POST',
+        url: '/marketplace',
+        data: {
+            name: this.name,
+            email: this.email,
+            phone: this.phone,
+            email: this.email
+          }
+      }).then(function(response){
+          console.log(response);
+      }, function(){
+          console.log('error');
+      });
+  }
+
 }]);
--- server.js
+++ server.js
@@ -12,19 +12,19 @@ const PORT =  process.env.PORT || 3000;
 //mongoose
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/project';
 
-// connect to mongo
-mongoose.connect(MONGODB_URI ,  { useNewUrlParser: true});
-db.on('open', () => {
-  console.log('connected to mongo');
-});
-
-
 //=============
 // MIDDLEWARE
 //=============
 app.use(express.static('public'));
 app.use(express.json());
 app.use('/marketplace', marketplaceController);
+
+
+// connect to mongo
+mongoose.connect(MONGODB_URI ,  { useNewUrlParser: true});
+db.on('open', () => {
+  console.log('connected to mongo');
+});
 
 
 //=============
