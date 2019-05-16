0644d80c6d689b5a2882bc21a46f42879c5bf643
Aiello <aiello@aiellos-mbp.attlocal.net>
2019-05-16 17:47:46 -0400

    Added post, delete, and put routes to controller.js.

--- controllers/controller.js
+++ controllers/controller.js
@@ -15,5 +15,31 @@ router.get('/', (req, res) => {
   });
 });
 
+//=======
+// POST
+//=======
+router.post('/', (req, res) => {
+  User.create(req.body, (err, createdUser) => {
+    res.json(createdUser)
+  });
+});
+
+//=========
+// DELETE
+//=========
+router.delete('/:id', (req, res)=>{
+    User.findByIdAndRemove(req.params.id, (err, deletedUser) => {
+        res.json(deletedUser);
+    });
+});
+
+//=========
+// UPDATE
+//=========
+router.put('/:id', (req, res)=>{
+    User.findByIdAndUpdate(req.params.id, req.body, {new:true}, (err, updatedUser)=>{
+        res.json(updatedUser);
+    });
+});
 
 module.exports = router;
