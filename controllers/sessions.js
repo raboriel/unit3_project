987160e985b4a3959b12344ca02429a2999f9f22
Kyungsub Kim <raboriel@gmail.com>
2019-05-16 20:43:10 -0400

    use bcryptjs instead of bcrypt

--- controllers/sessions.js
+++ controllers/sessions.js
@@ -3,9 +3,7 @@ const router = express.Router();
 const User = require('../models/users.js');
 const bcrypt = require('bcryptjs');
 
-// router.get('/new', (req, res)=>{
-//     res.render('sessions/new.ejs');
-// })
+
 
 router.delete('/', (req, res)=>{
     req.session.destroy(() => {
