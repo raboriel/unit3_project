b8838a06b9ed10add38aee1b1ff42270d20d7683
Kyungsub Kim <raboriel@gmail.com>
2019-05-17 19:16:23 -0400

    create and log in work, also hide text box

--- models/users.js
+++ models/users.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = Schema({
-    username: String,
+    username: { type: String, required: true, unique: true },
     password: String,
     email: String,
     Phone: Number,
